df7215df7653ebe61bb2e14072b44df6d18f67ac
Nathan Thomas <nwthomas@me.com>
2019-01-02 12:07:03 -0800

    Add default user thumbnail pic

--- client/src/Component/NavbarContainer/NavbarThumbnail.js
+++ client/src/Component/NavbarContainer/NavbarThumbnail.js
@@ -1,11 +1,12 @@
 import React from "react";
+import defaultPicture from "../../Images/cat__default__profile.jpg";
 
 const NavbarThumbnail = props => {
   return (
     <div className="navbar__thumbnail">
       <img
         className="navbar__username__avatar"
-        src="../../Images/cat__default__profile.jpg"
+        src={defaultPicture}
         alt="User thumbnail"
       />
       <a className="navbar__username__link" href="index.html">
