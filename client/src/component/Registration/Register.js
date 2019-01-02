400a308aa245dbf111302b499e0ef18bed8f19db
Nathan Thomas <nwthomas@me.com>
2019-01-02 11:22:47 -0800

    Add BEM styles classnames for registration form

--- client/src/component/Registration/Register.js
+++ client/src/component/Registration/Register.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Register extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstname: '',
-      lastname: '',
-      username: '',
-      password: '',
-      email: '',
-      avatar: '',
-      location: '',
-      creds: '',
-      cats: '',
-      role: ''
+      firstname: "",
+      lastname: "",
+      username: "",
+      password: "",
+      email: "",
+      avatar: "",
+      location: "",
+      creds: "",
+      cats: "",
+      role: ""
     };
   }
 
@@ -22,35 +22,86 @@ export default class Register extends Component {
   render() {
     return (
       <div>
-        <div className='register_container'>
+        <div className="register__container">
           <form>
-            <input type='text' name='username' placeholder='Username' />
+            <input
+              className="register__username"
+              type="text"
+              name="username"
+              placeholder="Username"
+            />
             <br />
-            <input type='text' name='firstname' placeholder='First name' />
-            <input type='text' name='lastname' placeholder='Last name' />
+            <input
+              className="register__firstname"
+              type="text"
+              name="firstname"
+              placeholder="First name"
+            />
+            <input
+              className="register__lastname"
+              type="text"
+              name="lastname"
+              placeholder="Last name"
+            />
             <br />
-            <input type='password' name='password' placeholder='Password' />
+            <input
+              className="register__password"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
             <br />
-            <input type='email' name='email' placeholder='Email' />
+            <input
+              className="register__email"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
             <br />
-            <input type='text' name='avatar' placeholder='Avatar url' />
+            <input
+              className="register__avatar"
+              type="text"
+              name="avatar"
+              placeholder="Avatar url"
+            />
             <br />
-            <input type='date' name='dob' min='1940-01-01' max='2019-01-02' />
-            <input type='text' name='location' placeholder='Location' />
+            <input
+              className="register__dob"
+              type="date"
+              name="dob"
+              min="1940-01-01"
+              max="2019-01-02"
+            />
+            <input
+              className="register__location"
+              type="text"
+              name="location"
+              placeholder="Location"
+            />
             <br />
 
-            <input type='text' name='creds' placeholder='Credentials' />
+            <input
+              className="register__credentials"
+              type="text"
+              name="creds"
+              placeholder="Credentials"
+            />
             <br />
-            <select name='cats'>
-              <option value='taxes'>Taxes</option>
-              <option value='investing'>Investing</option>
-              <option value='estate'>Estate Planning</option>
-              <option value='financial'>Financial Planning</option>
+            <select className="register__role-list" name="cats">
+              <option value="taxes">Taxes</option>
+              <option value="investing">Investing</option>
+              <option value="estate">Estate Planning</option>
+              <option value="financial">Financial Planning</option>
             </select>
 
-            <input type='text' name='role' placeholder='Role' />
+            <input
+              className="register__role-input"
+              type="text"
+              name="role"
+              placeholder="Role"
+            />
             <div>
-              <button>Register</button>
+              <button className="register__button button">Register</button>
             </div>
           </form>
         </div>
