ce0465acc676a92154fc73f53662dd27e62e99c4
joepound <14357797+joepound@users.noreply.github.com>
2019-01-02 15:20:52 -0700

    Updated styling for Login component

--- client/src/Component/Login/Login.js
+++ client/src/Component/Login/Login.js
@@ -1,25 +1,52 @@
 import React from 'react';
+import './Login.css';
 
 const Login = props => {
   return (
     <div>
       <div className='login__container'>
         <form className='login__form' onSubmit={props.logInUser}>
-          <input
-            className='login__username'
-            type='text'
-            name='username'
-            placeholder='Username'
-            handleChange={props.handleChange}
-          />
-          <input
-            className='login__password'
-            type='text'
-            name='password'
-            placeholder='Password'
-            handleChange={props.handleChange}
-          />
-          <button type='submit'>Log in</button>
+          <h2
+            className="login__form__heading">
+            Log In
+          </h2>
+          <div
+            className='login__form__field'
+          >
+            <span
+              className='login__form__field__label'
+            >
+              Username
+            </span>
+            <input
+              className='login__form__field__text'
+              type='text'
+              name='username'
+              placeholder='Enter your username'
+              handleChange={props.handleChange}
+            />
+          </div>
+          <div
+            className='login__form__field'
+          >
+            <span
+              className='login__form__field__label'
+            >
+              Password
+            </span>
+            <input
+              className='login__form__field__text'
+              type='password'
+              name='password'
+              placeholder='Enter your password'
+              handleChange={props.handleChange}
+            />
+          </div>
+          <button 
+            className='login__form__button'
+            type='submit'>
+            Log in
+          </button>
         </form>
       </div>
     </div>
