efd73f59f790433aa5314dbdd6d613ba5c9d2e86
joepound <14357797+joepound@users.noreply.github.com>
2019-01-02 16:12:06 -0700

    Updated styling for Registration component

--- client/src/Component/Login/Login.js
+++ client/src/Component/Login/Login.js
@@ -1,60 +1,58 @@
 import React from 'react';
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import './Login.css';
 
 const Login = props => {
   return (
-    <div>
-      <div className='login__container'>
-        <form className='login__form' onSubmit={props.logInUser}>
-          <h2
-            className="login__form__heading">
-            Member Login
-          </h2>
-          <div
-            className='login__form__field'
+    <div className='login__container'>
+      <form className='login__form' onSubmit={props.logInUser}>
+        <h2
+          className='login__form__heading'>
+          Member Login
+        </h2>
+        <div
+          className='login__form__field'
+        >
+          <span
+            className='login__form__field__label'
           >
-            <span
-              className='login__form__field__label'
-            >
-              Username
-            </span>
-            <input
-              className='login__form__field__text'
-              type='text'
-              name='username'
-              placeholder='Enter your username'
-              handleChange={props.handleChange}
-            />
-          </div>
-          <div
-            className='login__form__field'
+            Username
+          </span>
+          <input
+            className='login__form__field__text'
+            type='text'
+            name='username'
+            placeholder='Enter your username'
+            handleChange={props.handleChange}
+          />
+        </div>
+        <div
+          className='login__form__field'
+        >
+          <span
+            className='login__form__field__label'
           >
-            <span
-              className='login__form__field__label'
-            >
-              Password
-            </span>
-            <input
-              className='login__form__field__text'
-              type='password'
-              name='password'
-              placeholder='Enter your password'
-              handleChange={props.handleChange}
-            />
-          </div>
-          <button 
-            className='login__form__button'
-            type='submit'>
-            Log In
-          </button>
-          <div
-            className="login__form__register"
-          >
-          Not registered yet? <NavLink to="/register" className="login__form__register__link">Create an account</NavLink>
-          </div>
-        </form>
-      </div>
+            Password
+          </span>
+          <input
+            className='login__form__field__text'
+            type='password'
+            name='password'
+            placeholder='Enter your password'
+            handleChange={props.handleChange}
+          />
+        </div>
+        <button 
+          className='login__form__button'
+          type='submit'>
+          Log In
+        </button>
+        <div
+          className='login__form__register'
+        >
+        Not registered yet? <NavLink to='/register' className='login__form__register__link'>Create an account</NavLink>
+        </div>
+      </form>
     </div>
   );
 };
