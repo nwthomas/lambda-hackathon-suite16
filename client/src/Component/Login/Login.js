1584e333f014653cdd1bc82526a62eb490ecac0b
Nathan Thomas <nwthomas@me.com>
2019-01-02 15:33:48 -0800

    Update login.js

--- client/src/Component/Login/Login.js
+++ client/src/Component/Login/Login.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Login = props => {
   return (
     <div>
-      <div className='login__container'>
-        <form className='login__form' onSubmit={props.logInUser}>
+      <div className="login__container">
+        <form className="login__form" onSubmit={props.logInUser}>
           <input
-            className='login__username'
-            type='text'
-            name='username'
-            placeholder='Username'
+            className="login__username"
+            type="text"
+            name="username"
+            placeholder="Username"
             handleChange={props.handleChange}
           />
           <input
-            className='login__password'
-            type='text'
-            name='password'
-            placeholder='Password'
+            className="login__password"
+            type="text"
+            name="password"
+            placeholder="Password"
             handleChange={props.handleChange}
           />
-          <button type='submit'>Log in</button>
+          <button type="submit">Log in</button>
         </form>
       </div>
     </div>
