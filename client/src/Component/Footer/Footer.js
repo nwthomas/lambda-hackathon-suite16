12fabf03282f2ca94fc170d7bd5fbe637563bdaf
cecil john tantay <cjbtantay@gmail.com>
2019-01-02 22:38:45 -0800

    added styles in footer

--- client/src/Component/Footer/Footer.js
+++ client/src/Component/Footer/Footer.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import './Footer.css'
+import Logo from './footerlogo.svg'
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer__container">
-        <div className="section section1">
-          <h2>section 1</h2>
+      <div className="footer__outer__container">
+        <div className="footer__container">
+          <div className="section section1">
+            <h2>section 1</h2>
+          </div>
+          <div className="section section2">
+            <h2>section 2</h2>
+          </div>
+          <div className="section section3">
+            <h2>section 3</h2>
+          </div>
         </div>
-        <div className="section section2">
-          <h2>section 2</h2>
+        <div className='footer__copyright__right'>
+          <img className='footer_logo' src={Logo} alt="" />
+          <div className='footer__copyright'>© 2018 SUITE_16</div>
         </div>
-        <div className="section section3">
-          <h2>section 3</h2>
-        </div>
+
       </div>
-      <div className='footer__copyright'>© 2018 SUITE_16</div>
+
     </div>
   )
 }
