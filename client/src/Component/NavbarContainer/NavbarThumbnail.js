089d35bb3733e654ba94d1cdfb04248d971b836c
joepound <14357797+joepound@users.noreply.github.com>
2019-01-02 22:05:12 -0700

    Conditional rendering for nav logo and avatar photo

--- client/src/Component/NavbarContainer/NavbarThumbnail.js
+++ client/src/Component/NavbarContainer/NavbarThumbnail.js
@@ -1,15 +1,19 @@
 import React from "react";
+import navLogo from "../../Images/navlogo.svg";
 import defaultPicture from "../../Images/cat__default__profile.jpg";
 import "./Navbar.css";
 
 const NavbarThumbnail = props => {
   return (
     <div className="navbar__thumbnail">
-      <div className="navbar__username__avatar">
+      <div className={localStorage.token ? "navbar__logo-image--hidden" : "navbar__logo-image"}>
+        <img src={navLogo} alt="Company logo" />
+      </div>
+      <div className={localStorage.token ? "navbar__username__avatar" : "navbar__username__avatar--hidden"}>
         <img src={defaultPicture} alt="User thumbnail" />
       </div>
       <a className="navbar__username__link" href="/">
-        {!props.loggedInUser ? null : props.loggedInUser}
+        {localStorage.token && props.loggedInUser ? props.loggedInUser : "SUITE_16"}
       </a>
     </div>
   );
