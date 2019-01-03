f8ca2a4b329f4b4724b75088bdedcac62eaddcf0
cecil john tantay <cjbtantay@gmail.com>
2019-01-02 21:38:56 -0800

    added footer maxwidth

--- client/src/Component/Footer/Footer.js
+++ client/src/Component/Footer/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className="footer__container">
-      <h1>THIS IS THE FOOTER</h1>
+    <div className="footer">
+      <div className="footer__container">
+        <div className="section section1">
+          <h2>section 1</h2>
+        </div>
+        <div className="section section2">
+          <h2>section 2</h2>
+        </div>
+        <div className="section section3">
+          <h2>section 3</h2>
+        </div>
+      </div>
       <div className='footer__copyright'>© 2018 SUITE_16</div>
     </div>
   )
 }
 
 export default Footer
+
