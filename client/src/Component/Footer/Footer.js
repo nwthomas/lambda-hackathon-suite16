2f5418459ae8f9dc7349db16b00c12df12e7ad74
cecil john tantay <cjbtantay@gmail.com>
2019-01-02 22:50:27 -0800

    more styles on footer

--- client/src/Component/Footer/Footer.js
+++ client/src/Component/Footer/Footer.js
@@ -1,32 +1,23 @@
-import React from 'react'
-import './Footer.css'
-import Logo from './footerlogo.svg'
+import React from "react";
+import "./Footer.css";
+import Logo from "./footerlogo.svg";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer__outer__container">
         <div className="footer__container">
-          <div className="section section1">
-            <h2>section 1</h2>
-          </div>
-          <div className="section section2">
-            <h2>section 2</h2>
-          </div>
-          <div className="section section3">
-            <h2>section 3</h2>
-          </div>
+          <div className="section section1" />
+          <div className="section section2" />
+          <div className="section section3" />
         </div>
-        <div className='footer__copyright__right'>
-          <img className='footer_logo' src={Logo} alt="" />
-          <div className='footer__copyright'>© 2018 SUITE_16</div>
+        <div className="footer__copyright__right">
+          <img className="footer_logo" src={Logo} alt="" />
+          <div className="footer__copyright">© 2018 SUITE_16</div>
         </div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
--- client/src/Component/NavbarContainer/LoginRegistration.js
+++ client/src/Component/NavbarContainer/LoginRegistration.js
@@ -6,13 +6,49 @@ const LoginRegistration = props => {
   return (
     <div className="login__registration">
       <NavLink className="navlink" to="/login">
-        <button className={localStorage.token ? "navbar__button--hidden" : "login__button navbar__button"}>Login</button>
+        <button
+          className={
+            localStorage.token
+              ? "navbar__button--hidden"
+              : "login__button navbar__button"
+          }
+        >
+          Login
+        </button>
       </NavLink>
       <NavLink className="navlink" to="/register">
-        <button className={localStorage.token ? "navbar__button--hidden" : "register__button navbar__button"}>Register</button>
+        <button
+          className={
+            localStorage.token
+              ? "navbar__button--hidden"
+              : "register__button navbar__button"
+          }
+        >
+          Register
+        </button>
       </NavLink>
       <NavLink className="navlink" to="/">
-        <button className={localStorage.token ? "logout__button navbar__button" : "navbar__button--hidden"} onClick={props.logOutUser}>Log Out</button>
+        <button
+          className={
+            localStorage.token
+              ? "logout__button navbar__button"
+              : "navbar__button--hidden"
+          }
+          onClick={props.logOutUser}
+        >
+          Log Out
+        </button>
+      </NavLink>
+      <NavLink className="navlink" to="/">
+        <button
+          className={
+            localStorage.token
+              ? "logout__button navbar__button"
+              : "navbar__button--hidden"
+          }
+        >
+          Chat
+        </button>
       </NavLink>
     </div>
   );
