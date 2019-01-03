d3fa0edcdf31eac20d7f1c94cade9eaafc7e14e4
Nathan Thomas <nwthomas@me.com>
2019-01-03 15:54:25 -0800

    Build initial profile list components

--- client/src/Component/Dashboard/Dashboard.js
+++ client/src/Component/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Profile from "../Profile/Profile";
 import VideoChat from "../VideoChat/VideoChat";
+import ProfileListsContainer from "../ProfileLists/ProfileListsContainer";
 
 const Dashboard = props => {
   return (
     <div>
       <Profile _id={props._id} profiles={props.profiles} />
+      <ProfileListsContainer profiles={props.profiles} />
       <VideoChat />
     </div>
   );
---
+++ client/src/Component/ProfileLists/ProfileListsContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProfileLists.css";
+
+const ProfileListsContainer = props => {
+  return <div className="profile-lists__container" />;
+};
+
+export default ProfileListsContainer;
