fb46e5d94828e6810d5f134b1ee425469ed371f6
cecil john tantay <cjbtantay@gmail.com>
2019-01-03 21:22:11 -0800

    added iframe records

--- client/src/Component/Records/Records.js
+++ client/src/Component/Records/Records.js
@@ -10,6 +10,7 @@ const Records = () => {
         src="https://data.gov.au/dataset/asic-financial-adviser/resource/2156cb99-3358-4847-8b5b-fcd2f0d3c4e2/view/35f9d8fa-5215-4bf6-a83b-3b2091e66231"
         frameBorder="0"
       />
+      
     </div>
   );
 };
