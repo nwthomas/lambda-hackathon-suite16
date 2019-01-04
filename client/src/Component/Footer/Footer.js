807b78183730549da4e8684701f665101fbadecd
cacheUp <bradb26@gmail.com>
2019-01-04 09:36:21 +0300

    water... .splash

--- client/src/Component/Footer/Footer.js
+++ client/src/Component/Footer/Footer.js
@@ -8,25 +8,26 @@ const Footer = () => {
       <div className="footer__outer__container">
         <div className="footer__container">
           <div className="section section1">
-            <h2>Quick Links</h2>
-            <p>About us</p>
+            <h2 className="dad__foot">Quick Links</h2>
+
             <p>Sign-up</p>
             <p>Login</p>
           </div>
           <div className="section section2">
-            <h2>Services</h2>
+            <h2 className="dad__foot">Services</h2>
             <p>Video Chat</p>
             <p>Meet an advisor</p>
           </div>
           <div className="section section3">
-            <h2>Get in Touch</h2>
+            <h2 className="dad__foot">Get in Touch</h2>
             <p>Meet the team</p>
             <p>Careers</p>
           </div>
-        </div>
-        <div className="footer__copyright__right">
-          <img className="footer_logo" src={Logo} alt="" />
-          <div className="footer__copyright">© 2018 SUITE_16</div>
+
+          <div className="footer__copyright__right">
+            <img className="footer_logo" src={Logo} alt="" />
+            <div className="footer__copyright">© 2018 SUITE_16</div>
+          </div>
         </div>
       </div>
     </div>
