284a58d6314440577d8765579ecc9f71cdbb77d5
Nathan Thomas <nwthomas@me.com>
2019-01-03 16:11:44 -0800

    Revert "Perfessional. "

--- client/src/Component/Home/HomeCategory.js
+++ client/src/Component/Home/HomeCategory.js
@@ -7,34 +7,32 @@ import "./Home.css";
 const HomeCategory = props => {
   return (
     <div className="icon__wrapper">
-      <div className="icon__container">
-        <div className="icon__item item__a">
-          <img src={chart} className="category__icon" alt="icon" />
-          <h2 className="category__text">Invest</h2>
-        </div>
+      <div className="icon__item item__a">
+        <img src={chart} className="category__icon" alt="icon" />
+        <h2 className="category__text">Invest</h2>
+      </div>
 
-        <div className="icon__item item__b">
-          <img src={holdingMoney} className="category__icon" alt="icon" />
-          <h2 className="category__text">Taxes</h2>
-        </div>
+      <div className="icon__item item__b">
+        <img src={holdingMoney} className="category__icon" alt="icon" />
+        <h2 className="category__text">Taxes</h2>
+      </div>
 
-        <div className="icon__item item__c">
-          <img src={home} className="category__icon" alt="icon" />
-          <h2 className="category__text">
-            Estate
-            <br />
-            Management
-          </h2>
-        </div>
+      <div className="icon__item item__c">
+        <img src={home} className="category__icon" alt="icon" />
+        <h2 className="category__text">
+          Estate
+          <br />
+          Management
+        </h2>
+      </div>
 
-        <div className="icon__item item__d">
-          <img src={moneyBill} className="category__icon" alt="icon" />
-          <h2 className="category__text">
-            Financial
-            <br />
-            Management
-          </h2>
-        </div>
+      <div className="icon__item item__d">
+        <img src={moneyBill} className="category__icon" alt="icon" />
+        <h2 className="category__text">
+          Financial
+          <br />
+          Management
+        </h2>
       </div>
     </div>
   );
